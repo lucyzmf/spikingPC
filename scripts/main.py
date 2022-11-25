8df4aebfdf5d5abd1942c9c811ad247dea3ed127
lucyzmf <lucy.zmf@gmail.com>
2022-11-25 17:40:06 +0100

    reformat style

--- scripts/main.py
+++ scripts/main.py
@@ -41,16 +41,16 @@ transform = transforms.Compose(
 batch_size = 128
 
 traindata = torchvision.datasets.MNIST(root='./data', train=True,
-                                        download=True, transform=transform)
+                                       download=True, transform=transform)
 
 testdata = torchvision.datasets.MNIST(root='./data', train=False,
-                                       download=True, transform=transform)
+                                      download=True, transform=transform)
 
 # data loading 
 train_loader = torch.utils.data.DataLoader(traindata, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
+                                           shuffle=True, num_workers=2)
 test_loader = torch.utils.data.DataLoader(testdata, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)                                          
+                                          shuffle=True, num_workers=2)
 
 # check data loading correctness
 for batch_idx, (data, target) in enumerate(train_loader):
@@ -59,9 +59,11 @@ for batch_idx, (data, target) in enumerate(train_loader):
 
 # %%
 # set input and t param
-IN_dim = 28*28
+IN_dim = 28 * 28
 T = 20  # sequence length, reading from the same image T times 
-# if apply first layer drop out, creates sth similar to poisson encoding 
+
+
+# if apply first layer drop out, creates sth similar to poisson encoding
 
 # %%
 ###############################################################################################
@@ -74,7 +76,7 @@ def test(model, test_loader):
     correct = 0
 
     # for data, target in test_loader:
-    for i ,(data, target) in enumerate(test_loader):
+    for i, (data, target) in enumerate(test_loader):
         data, target = data.to(device), target.to(device)
         data = data.view(-1, IN_dim)
 
@@ -82,34 +84,36 @@ def test(model, test_loader):
             model.eval()
             hidden = model.init_hidden(data.size(0))
 
-            outputs, hidden= model(data, hidden, T) 
-           
+            outputs, hidden = model(data, hidden, T)
+
             output = outputs[-1]
             # output = torch.stack(outputs[-10:]).mean(dim=0)
-            
+
             test_loss += F.nll_loss(output, target, reduction='sum').data.item()
             pred = output.data.max(1, keepdim=True)[1]
-        
+
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
         torch.cuda.empty_cache()
 
     test_loss /= len(test_loader.dataset)
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-           test_loss, correct, len(test_loader.dataset),
-           100. * correct / len(test_loader.dataset)))
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
     return test_loss, 100. * correct / len(test_loader.dataset)
+
 
 ###############################################################################################
 ##########################          Train function             ###############################
 ###############################################################################################
 # training parameters
-K  = T # K is num updates per sequence 
-omega = int(T/K)  # update frequency 
+K = T  # K is num updates per sequence
+omega = int(T / K)  # update frequency
 clip = 1.
 log_interval = 100
 lr = 1e-3
 epoch = 30
 n_classes = 10
+
 
 # train function for one epoch
 def train(train_loader, n_classes, model, named_params):
@@ -126,62 +130,61 @@ def train(train_loader, n_classes, model, named_params):
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         data = data.view(-1, IN_dim)
-       
+
         B = target.size()[0]
 
         for p in range(T):
 
-            if p==0:
+            if p == 0:
                 h = model.init_hidden(data.size(0))
-            elif p%omega==0:
+            elif p % omega == 0:
                 h = tuple(v.detach() for v in h)
 
-            
-            o, h,hs = model.network.forward(data, h )
+            o, h, hs = model.network.forward(data, h)
 
             prob_out = F.softmax(h[-1], dim=1)
-            output = F.log_softmax(h[-1], dim=1) 
+            output = F.log_softmax(h[-1], dim=1)
 
-    
-            if p%omega==0 and p>0: 
+            if p % omega == 0 and p > 0:
                 optimizer.zero_grad()
-                
+
                 # classification loss
-                clf_loss = (p+1)/(K)*F.nll_loss(output, target)
+                clf_loss = (p + 1) / (K) * F.nll_loss(output, target)
                 # clf_loss = snr*F.cross_entropy(output, target,reduction='none')
                 # clf_loss = torch.mean(clf_loss)
 
                 # energy loss 
-                    
-                regularizer = get_regularizer_named_params( named_params, _lambda=1.0 )      
-                loss = clf_loss  + regularizer 
+
+                regularizer = get_regularizer_named_params(named_params, _lambda=1.0)
+                loss = clf_loss + regularizer
 
                 loss.backward()
 
                 if clip > 0:
                     torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
-                    
-                    
+
                 optimizer.step()
-                post_optimizer_updates( named_params)
-            
+                post_optimizer_updates(named_params)
+
                 train_loss += loss.item()
                 total_clf_loss += clf_loss.item()
-                total_regularizaton_loss += regularizer #.item()
-        
+                total_regularizaton_loss += regularizer  # .item()
+
         if batch_idx > 0 and batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tlr: {:.6f}\tLoss: {:.6f}\tOracle: \
                 {:.6f}\tClf: {:.6f}\tReg: {:.6f}\tFr: {:.6f}'.format(
-                   epoch, batch_idx * batch_size, len(train_loader.dataset),
-                   100. * batch_idx / len(train_loader), lr, train_loss / log_interval, 
-                   total_oracle_loss / log_interval, 
-                   total_clf_loss / log_interval, total_regularizaton_loss / log_interval, model.network.fr/T/log_interval))
+                    epoch, batch_idx * batch_size, len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), lr, train_loss / log_interval,
+                    total_oracle_loss / log_interval,
+                    total_clf_loss / log_interval, total_regularizaton_loss / log_interval,
+                    model.network.fr / T / log_interval))
             # print(model.network.fr)
             train_loss = 0
             total_clf_loss = 0
             total_regularizaton_loss = 0
             total_oracle_loss = 0
         model.network.fr = 0
+
 
 # %%
 ###############################################################
@@ -194,9 +197,6 @@ model = one_layer_SeqModel(IN_dim, 784, n_classes, is_rec=True, is_LTC=False)
 model.to(device)
 print(model)
 
-
-
-
 # %%
 # define new loss and optimiser 
 total_params = count_parameters(model)
@@ -205,7 +205,7 @@ print('total param count %i' % total_params)
 # define optimiser
 optimizer = optim.Adamax(model.parameters(), lr=lr, weight_decay=0.0001)
 # reduce the learning after 20 epochs by a factor of 10
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)  
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
 
 # %%
 ###############################################################################################
@@ -213,32 +213,29 @@ scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
 ###############################################################################################
 
 # untrained network
-test_loss, acc1 = test( model, test_loader )
+test_loss, acc1 = test(model, test_loader)
 
 # %%
 epochs = 30
-named_params = get_stats_named_params( model )
-prefix ='save name'
+named_params = get_stats_named_params(model)
+prefix = 'save name'
 all_test_losses = []
 best_acc1 = 20
 
 estimate_class_distribution = torch.zeros(n_classes, T, n_classes, dtype=torch.float)
 for epoch in range(epochs):
-    train(train_loader, n_classes, model, named_params)   
+    train(train_loader, n_classes, model, named_params)
 
     reset_named_params(named_params)
 
-
-    test_loss, acc1 = test( model, test_loader )
-
+    test_loss, acc1 = test(model, test_loader)
 
     scheduler.step()
 
-        
     # remember best acc@1 and save checkpoint
     is_best = acc1 > best_acc1
     best_acc1 = max(acc1, best_acc1)
-        
+
     # save_checkpoint({
     #         'epoch': epoch + 1,
     #         'state_dict': model.state_dict(),
@@ -250,5 +247,5 @@ for epoch in range(epochs):
 
     all_test_losses.append(test_loss)
 
-test_loss, acc1 = test( model, test_loader )
+test_loss, acc1 = test(model, test_loader)
 # %%
