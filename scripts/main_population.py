8511358fcd3d3dc6ec61dfca44e908c77a161f99
Lucy Zhang <67420952+lucyzmf@users.noreply.github.com>
2023-01-24 22:20:03 +0100

    l1 reg

--- scripts/main_population.py
+++ scripts/main_population.py
@@ -50,7 +50,7 @@ input_scale = config.input_scale
 config.lr = 1e-3
 
 # experiment name 
-exp_name = 'ener_included_out_onetoone'
+exp_name = 'ener_included_out_onetoone_l1reg'
 energy_penalty = True
 spike_loss = config.spike_loss
 adap_neuron = config.adap_neuron
