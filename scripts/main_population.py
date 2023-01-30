c79a95beb2e6991c70a28fa425f3f79cb43a1468
lucyzmf <lucy.zmf@gmail.com>
2023-01-30 14:24:03 +0100

    go back to fc rec imple

--- scripts/main_population.py
+++ scripts/main_population.py
@@ -208,7 +208,7 @@ def train(train_loader, n_classes, model, named_params):
                     energy = h[1].mean()  # * 0.1
                 else:
                     # mem potential loss take l1 norm / num of neurons /batch size
-                    energy = (torch.norm(h[0], p=1) + torch.norm(h[3], p=1)) / B / 784
+                    energy = (torch.norm(h[3], p=1) + torch.norm(h[6], p=1)) / B / 784
 
                 # l1 loss on rec weights 
                 # l1_norm = torch.linalg.norm(model.network.snn_layer.layer1_x.weight)
@@ -276,7 +276,7 @@ def train(train_loader, n_classes, model, named_params):
 ###############################################################
 # set input and t param
 IN_dim = 784
-hidden_dim = [10 * config.num_readout, 784]
+hidden_dim = [256, [10 * config.num_readout, 784]]
 T = 20  # sequence length, reading from the same image T times
 
 # define network
