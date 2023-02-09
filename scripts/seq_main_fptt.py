367f3366ecdbae7760cb0995daaad3f3aa4c24dd
lucyzmf <lucy.zmf@gmail.com>
2023-02-09 11:50:47 +0100

    train script for seq data

---
+++ scripts/seq_main_fptt.py
@@ -0,0 +1,195 @@
+# %%
+# this script contains training with sequence data
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchaudio
+
+from torch.nn.parameter import Parameter
+from torch.nn import init
+from torch.autograd import Variable
+import torchvision.transforms as transforms
+import torchvision
+import numpy as np
+import wandb
+from datetime import date
+import os
+
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+
+from tqdm import tqdm
+
+from network_class import *
+from utils import *
+from FTTP import *
+from sequence_dataset import *
+from test_function import test_seq
+
+# %%
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+# set seed
+torch.manual_seed(999)
+
+# wandb login
+wandb.login(key='25f10546ef384a6f1ab9446b42d7513024dea001')
+wandb.init(project="spikingPC_onelayer", entity="lucyzmf")
+# wandb.init(mode="disabled")
+
+# add wandb.config
+config = wandb.config
+
+# network hypers
+config.adap_neuron = True  # whether use adaptive neuron or not
+config.dp = 0.5
+config.onetoone = True
+config.num_readout = 10
+
+# loss hypers
+config.clf_alpha = 1  # proportion of clf loss
+config.energy_alpha = 1  # - config.clf_alpha
+
+# training alg hypers
+config.lr = 1e-3
+config.alg = 'fptt'
+alg = config.alg
+config.k_updates = 20
+
+# seq data set config
+config.seq_data = True  # whether applies sequence data
+seq_data = config.seq_data
+config.seq_len = 20  # sequence length
+config.random_switch = False  # predictable or random switch time
+config.switch_time = [config.seq_len / 2]  # if not random switch, provide switch time
+config.num_switch = 1  # used when random switch=T
+
+# training parameters
+T = config.seq_len
+K = config.k_updates  # k_updates is num updates per sequence
+omega = int(T / K)  # update frequency
+clip = 1.
+log_interval = 10
+epoch = 10
+n_classes = 10
+
+config.exp_name = config.alg + '_ener_dp05_poisson05thre_01alpha'
+
+# experiment name 
+exp_name = config.exp_name
+energy_penalty = True
+# checkpoint file name
+check_fn = 'onelayer_rec_best.pth.tar'
+# experiment date and name 
+today = date.today()
+# checkpoint file prefix 
+prefix = '../results/' + today.strftime("%b-%d-%Y") + '/' + exp_name + '/'
+
+# create exp path 
+if os.path.exists(prefix):
+    raise Exception('experiment dir already exist')
+else:
+    os.makedirs(prefix)
+
+# %%
+###############################################################
+# IMPORT DATASET 
+###############################################################
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5), (0.5))])
+
+batch_size = 256
+
+traindata = torchvision.datasets.MNIST(root='./data', train=True,
+                                       download=True, transform=transform)
+
+testdata = torchvision.datasets.MNIST(root='./data', train=False,
+                                      download=True, transform=transform)
+
+# generate sequence dataset
+seq_train = SequenceDataset(traindata.data, traindata.targets, config.seq_len, config.random_switch,
+                            config.switch_time, config.num_switch)
+seq_test = SequenceDataset(testdata.data, testdata.targets, config.seq_len, config.random_switch,
+                           config.switch_time, config.num_switch)
+
+print('image sequence data shape: ' + seq_train.img_datasize)
+print('label sequence data shape: ' + seq_train.label_size)
+
+train_loader = torch.utils.data.Dataloader(seq_train, batch_size=batch_size,
+                                           shuffle=True, num_workers=2)
+test_loader = torch.utils.data.DataLoader(seq_test, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+# %%
+###############################################################
+# DEFINE NETWORK
+###############################################################
+
+# set input and t param
+IN_dim = 784
+hidden_dim = [10 * config.num_readout, 784]
+
+# define network
+model = SnnNetwork(IN_dim, hidden_dim, n_classes, is_adapt=config.adap_neuron, one_to_one=config.onetoone,
+                   dp_rate=config.dp)
+model.to(device)
+print(model)
+
+# define new loss and optimiser 
+total_params = count_parameters(model)
+print('total param count %i' % total_params)
+
+# define optimiser
+optimizer = optim.Adamax(model.parameters(), lr=config.lr, weight_decay=0.0001)
+# reduce the learning after 20 epochs by a factor of 10
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.5)
+
+# %%
+###############################################################################################
+##########################          Training and testin         ###############################
+###############################################################################################
+
+# untrained network
+test_loss, acc1 = test_seq(model, test_loader, T)
+
+# %%
+
+epochs = 20
+named_params = get_stats_named_params(model)
+all_test_losses = []
+best_acc1 = 20
+
+wandb.watch(model, log_freq=100)
+
+for epoch in range(epochs):
+    train_fptt_seq(epoch, batch_size, log_interval, train_loader,
+                   model, named_params, T, K, omega, optimizer,
+                   config.clf_alpha, config.energy_alpha, clip, config.lr)
+
+    reset_named_params(named_params)
+
+    test_loss, acc1 = test_seq(model, test_loader, T)
+
+    scheduler.step()
+
+    # remember best acc@1 and save checkpoint
+    is_best = acc1 > best_acc1
+    best_acc1 = max(acc1, best_acc1)
+
+    if is_best:
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            # 'oracle_state_dict': oracle.state_dict(),
+            'best_acc1': best_acc1,
+            'optimizer': optimizer.state_dict(),
+            # 'oracle_optimizer' : oracle_optim.state_dict(),
+        }, is_best, prefix=prefix, filename=check_fn)
+
+    all_test_losses.append(test_loss)
+
+# %%
